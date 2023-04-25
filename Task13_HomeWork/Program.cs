567bde3412f5c793b4331790ea701928ba24a8c0
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 16:28:07 +0300

    Доб. решение задачи

--- Task13_HomeWork/Program.cs
+++ Task13_HomeWork/Program.cs
@@ -8,6 +8,7 @@
 
     Console.Write("Введите число: ");
         int number =  Convert.ToInt32(Console.ReadLine());
+        if (number<0) number *= -1;
         if (number >= 100 && number <= 999) 
          {
              int thirdDigit = number % 10;
