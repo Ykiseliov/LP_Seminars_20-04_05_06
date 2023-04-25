46ca471d5c4f6ea737f86a1038af6865648564cc
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 20:52:13 +0300

    Доб. решение задачи

--- Task 12/Program.cs
+++ Task 12/Program.cs
@@ -5,3 +5,24 @@
 // 34, 5 -> не кратно, остаток 4
 // 16, 4 -> кратно
 
+Console.Write("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+// int remainder = number1 % number2;
+// if (remainder == 0)
+// {
+//     Console.WriteLine("Кратно");
+// }
+// else
+// {
+//     Console.WriteLine($"Не кратно, остаток = {remainder}");
+// }
+int remainder = Remainder(number1 , number2);
+string result = remainder !=0 /*если не кратно*/ ? $"Не кратно, остаток = {remainder}": "кратно";
+Console.WriteLine(result);
+
+int Remainder (int num1, int num2)
+{
+    return num1 % num2;
+}
