91aa9f105bd6f945bae753bfcf65ba7792bdeeff
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 11:58:25 +0300

    доб решение задачи

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,5 +4,12 @@
 
 Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+if (number>=100&&number<=999)
+{
 int lastDigit = number%10;
 Console.WriteLine($"Ваше число {number}, его последняя цифра {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
