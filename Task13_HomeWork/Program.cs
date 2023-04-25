7cbb12b16f55fabbc29ba4b19683ed95445c3277
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 16:20:10 +0300

    Доб. решение задачи

--- Task13_HomeWork/Program.cs
+++ Task13_HomeWork/Program.cs
@@ -6,3 +6,24 @@
 
 // 32679 -> 6
 
+    Console.Write("Введите число: ");
+        int number =  Convert.ToInt32(Console.ReadLine());
+        if (number >= 100 && number <= 999) 
+         {
+             int thirdDigit = number % 10;
+             Console.WriteLine($"Третья цифра числа {number} будет {thirdDigit}");
+         }
+         else if (number >= 1000 && number <= 9999) {
+            int thirdDigit = (number / 10) %10;
+            Console.WriteLine($"Третья цифра числа {number} будет {thirdDigit}");
+        } 
+         else if (number >= 10000 && number <= 99999) 
+         {
+             int thirdDigit = (number / 100) % 10;
+             Console.WriteLine($"Третья цифра числа {number} будет {thirdDigit}");
+         }
+
+        else
+        {
+            Console.WriteLine("У числа нет третьей цифры.");
+        }
