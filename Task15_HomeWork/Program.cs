f951ed44b317287340759f8e1e3ad0edd1c8446f
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 16:31:30 +0300

    Доб. условие задачи

--- Task15_HomeWork/Program.cs
+++ Task15_HomeWork/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
