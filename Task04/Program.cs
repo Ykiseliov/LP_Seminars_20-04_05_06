6fb7debf9f36f9a71015e1189c1ddbc75302e3a7
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 12:01:45 +0300

    доб решение задачи

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,10 +4,11 @@
 
 Console.Write("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number>=100&&number<=999)
+if (number<0) number *= -1;
+if (number >= 100 && number <= 999)
 {
-int lastDigit = number%10;
-Console.WriteLine($"Ваше число {number}, его последняя цифра {lastDigit}");
+    int lastDigit = number % 10;
+    Console.WriteLine($"Ваше число {number}, его последняя цифра {lastDigit}");
 }
 else
 {
