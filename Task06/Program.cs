a2a187585d7cfab0b883376786c93d52cc67b0b0
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 13:41:43 +0300

    Доб. решение задачи

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -2,3 +2,14 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+    if(a%2 == 0)
+    {
+    Console.WriteLine("Число является четным.");
+    }
+    else
+    {
+    Console.WriteLine("Число является нечетным.");
+    }
