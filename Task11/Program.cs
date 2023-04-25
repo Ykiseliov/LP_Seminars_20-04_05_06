718fe1537656c0538d0418e3462635cc740fb39a
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 18:54:03 +0300

    Доб. решение задачи

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -4,3 +4,16 @@
 // 782 ->72
 // 918 -> 98
 
+int number = new Random().Next(100, 1000); //(10, 99 + 1)
+Console.WriteLine($"Случайное число из отрезка 100 - 1000 -> {number}");
+
+int RemSecDigit(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int ourDigit = firstDigit * 10 + thirdDigit;
+    return ourDigit;
+    // return firstDigit > secondDigit ? firstDigit:secondDigit;
+}   
+int result = RemSecDigit(number);
+Console.WriteLine($"{number} -> {result}");
