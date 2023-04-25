ddf5dcd44725d88c102878abd9f322452eb56c8b
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 18:02:05 +0300

    доб. папку Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(10, 100); //(10, 99 + 1)
+Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit:secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit:secondDigit;
+}   
