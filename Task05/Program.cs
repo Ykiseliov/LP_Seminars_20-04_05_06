df4ba5d59b9ad6d08ce0e2204f9cd23481e31666
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 12:16:54 +0300

    доб решение задачи

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+    for(int i = -number; i <= number; i++)
+    {
+        Console.WriteLine(i+ "");
+    }
