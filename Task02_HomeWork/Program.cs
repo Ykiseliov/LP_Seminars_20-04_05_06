2a809ce2cf1772d70c57414305b631d8254fe7e2
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 13:32:04 +0300

    Доб. решение задачи

--- Task02_HomeWork/Program.cs
+++ Task02_HomeWork/Program.cs
@@ -4,3 +4,20 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+ if (a > b)
+    {
+    Console.WriteLine($"Максимальное число:{a}");
+    Console.WriteLine($"Минимальное число: {b}");
+    }
+    else
+    {
+    Console.WriteLine($"Максимальное число:{b}");
+    Console.WriteLine($"Минимальное число: {a}");
+    }
