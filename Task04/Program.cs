3d5e2f7e2e110ed73e0aeeb14b237b17a33d6411
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 11:52:51 +0300

    доб решение задачи

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -3,3 +3,6 @@
 // этого числа.
 
 Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastDigit = number%10;
+Console.WriteLine($"Ваше число {number}, его последняя цифра {lastDigit}");
