b4b04dfd9cc11610c7200451e787e0d830212950
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 21:12:00 +0300

    Доб. решение задачи

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 14. Напишите программу, которая принимает на 
+// вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = Multiplicity(number);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool Multiplicity(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
