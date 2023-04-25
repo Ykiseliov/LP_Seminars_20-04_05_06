ae42e5a251eb224d6fd5c49b93cdb28283e49128
Ykiseliov <ykiseliov@gmail.com>
2023-04-25 21:28:15 +0300

    Доб. решение задачи

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -9,4 +9,10 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter 2 num: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
+bool result = IsSquare(number1, number2);
+Console.WriteLine(result ? "да":"нет");
 
+bool IsSquare (int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
