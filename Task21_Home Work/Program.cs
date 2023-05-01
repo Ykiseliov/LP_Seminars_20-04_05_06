b56fd2899c1783bbdfe7a0955b67434e612c74ad
Ykiseliov <ykiseliov@gmail.com>
2023-05-01 15:57:39 +0300

    upd.решение

--- Task21_Home Work/Program.cs
+++ Task21_Home Work/Program.cs
@@ -3,22 +3,22 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Enter coordinates of point 1:");
+Console.WriteLine("Координаты точки 1:");
             Console.Write("x: ");
-            double x1 = double.Parse(Console.ReadLine());
+            double x1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("y: ");
-            double y1 = double.Parse(Console.ReadLine());
+            double y1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("z: ");
-            double z1 = double.Parse(Console.ReadLine());
+            double z1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter coordinates of point 2:");
+            Console.WriteLine("Координаты точки 2:");
             Console.Write("x: ");
-            double x2 = double.Parse(Console.ReadLine());
+            double x2 = Convert.ToInt32(Console.ReadLine());
             Console.Write("y: ");
-            double y2 = double.Parse(Console.ReadLine());
+            double y2 = Convert.ToInt32(Console.ReadLine());
             Console.Write("z: ");
-            double z2 = double.Parse(Console.ReadLine());
+            double z2 = Convert.ToInt32(Console.ReadLine());
 
             double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
 
-            Console.WriteLine($"The distance between point 1 and point 2 is {distance:F2}");
+            Console.WriteLine($"Расстояние между 1 и 2 точками: {distance:F2}");
