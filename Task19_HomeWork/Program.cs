264ecdb1939483090737849f74ca061ea0434ab4
Ykiseliov <ykiseliov@gmail.com>
2023-05-01 14:43:34 +0300

    add решение

--- Task19_HomeWork/Program.cs
+++ Task19_HomeWork/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите пятизначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int first = number / 10000;
+        int second = number / 1000 % 10;
+        int fourth = number % 100 / 10;
+        int fifth = number % 10;
+
+        if (first == fifth && second == fourth)
+        {
+            Console.WriteLine("Число является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом.");
+        }
