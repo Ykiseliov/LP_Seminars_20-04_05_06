4678f98a7db79d37c11a823417761a51b23f7f8a
Ykiseliov <ykiseliov@gmail.com>
2023-05-01 15:44:34 +0300

    upd.решение

--- Task21_Home Work/Program.cs
+++ Task21_Home Work/Program.cs
@@ -3,3 +3,22 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+Console.WriteLine("Enter coordinates of point 1:");
+            Console.Write("x: ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("y: ");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.Write("z: ");
+            double z1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter coordinates of point 2:");
+            Console.Write("x: ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("y: ");
+            double y2 = double.Parse(Console.ReadLine());
+            Console.Write("z: ");
+            double z2 = double.Parse(Console.ReadLine());
+
+            double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+            Console.WriteLine($"The distance between point 1 and point 2 is {distance:F2}");
