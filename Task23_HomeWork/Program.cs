30bd53b3d9e5b792be9e77705cf43fb46ee55be1
Ykiseliov <ykiseliov@gmail.com>
2023-05-01 15:48:29 +0300

    add. условие

--- Task23_HomeWork/Program.cs
+++ Task23_HomeWork/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
