e4c90461c9c968e8f299dccf0f28017fe15b4bc4
Ykiseliov <ykiseliov@gmail.com>
2023-05-02 21:49:21 +0300

    upd.task

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -3,20 +3,15 @@
 // возможных координат точек в этой четверти (x и y)
 
 Console.WriteLine("Введите номер четверти:");
-int number = Convert.ToInt32(Console.ReadLine());
+string number = Console.ReadLine();
 string range = Range(number);
+Console.WriteLine(range == null ? "Некорректный ввод" : range);
 
-string result = range != "0"
-                ? $"Указанные координаты соответствуют четверти -> {range}"
-                :"Введены некорректные координаты";
-Console.WriteLine(result);
-
-
-string Range (int num)
+string Range (string num)
 {
-    if (num == 1) return "x > 0 && y > 0";
-    if (num == 2) return "x < 0 && y > 0" ;
-    if (num ==3) return "x < 0 && y < 0";
-    if (num == 4) return "x > 0 && y < 0";
-    return "0";
+    if (num == "1") return "x > 0 && y > 0";
+    if (num == "2") return "x < 0 && y > 0" ;
+    if (num == "3") return "x < 0 && y < 0";
+    if (num == "4") return "x > 0 && y < 0";
+    return null;
 }
