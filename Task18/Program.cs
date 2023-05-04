21b3982007e206c02e1cc39ebc3824f2a8e46ef6
Ykiseliov <ykiseliov@gmail.com>
2023-05-04 18:59:04 +0300

    upd.task

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,17 +1,17 @@
-﻿// Задача 18: Напишите программу, которая по
-// заданному номеру четверти, показывает диапазон
-// возможных координат точек в этой четверти (x и y)
+﻿// // Задача 18: Напишите программу, которая по
+// // заданному номеру четверти, показывает диапазон
+// // возможных координат точек в этой четверти (x и y)
 
-Console.WriteLine("Введите номер четверти:");
-string number = Console.ReadLine();
-string range = Range(number);
-Console.WriteLine(range == null ? "Некорректный ввод" : range);
+// Console.WriteLine("Введите номер четверти:");
+// // string number = Console.ReadLine() ;
+// string range = Range(number);
+// Console.WriteLine(range == null ? "Некорректный ввод" : range);
 
-string Range (string num)
-{
-    if (num == "1") return "x > 0 && y > 0";
-    if (num == "2") return "x < 0 && y > 0" ;
-    if (num == "3") return "x < 0 && y < 0";
-    if (num == "4") return "x > 0 && y < 0";
-    return null;
-}
+// string Range (string num)
+// {
+//     if (num == "1") return "x > 0 && y > 0";
+//     if (num == "2") return "x < 0 && y > 0" ;
+//     if (num == "3") return "x < 0 && y < 0";
+//     if (num == "4") return "x > 0 && y < 0";
+//     // return null;
+// }
