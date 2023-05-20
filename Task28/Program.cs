051fa599913228fbb8a8a9f8682b40a2df5e4dcf
Ykiseliov <ykiseliov@gmail.com>
2023-05-20 17:37:58 +0300

    Решение

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -3,3 +3,39 @@
 
 // 4 -> 24
 // 5 -> 120
+
+// Console.WriteLine("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int result = 1;
+// for (int i = 1; i <= n; i++)
+// {
+//     result *=i;
+// }
+// Console.WriteLine($"Произведение чисел от 1 до {n} равно {result}");
+
+//Второе решение
+Console.WriteLine("Bведите натуральное число N");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <=0) Console.WriteLine("введено не натуральное число");
+
+else
+{
+	int factorial = Factorial(number);
+	Console.WriteLine($"Факториал числа {number} равен {factorial}");
+}
+
+int Factorial(int num)
+{
+	int fact = 1;
+
+	for (int i=1; i <= num; i++)
+	{
+		checked
+		{
+			fact = fact * i;
+		}
+	}
+	return fact;
+}
