f26757134667b9f403c00bed11a639ddc64c9048
Ykiseliov <ykiseliov@gmail.com>
2023-05-20 14:56:47 +0300

    Task26

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+
+// 456-> 3
+// 78-> 2
+// 89126-> 5
