821efba95767f08846b682e0d65c0afd2824753e
Ykiseliov <ykiseliov@gmail.com>
2023-05-20 15:53:10 +0300

    Решение

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -5,5 +5,21 @@
 // 78-> 2
 // 89126-> 5
 
-Console.Write("Введите число: ");
-string input = Console.ReadLine();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = CountNumbers(number);
+
+Console.WriteLine($"Кол-во цифр в числе {number} = {count}");
+
+int CountNumbers (int num)
+{
+    int i = 0;
+    while (num !=0)
+    {
+        num = num / 10;
+        i++;
+    }
+    return i;
+}
+    
